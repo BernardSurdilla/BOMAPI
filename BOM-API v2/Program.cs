715163915b357e5ff03b54d501ecec9767016570
Francis-Romeo-Remolacio <145172462+Francis-Romeo-Remolacio@users.noreply.github.com>
2024-10-15 00:14:06 +0800

    Add CORS policy for custom header
    try lang
    idk if work ✌

--- BOM-API v2/Program.cs
+++ BOM-API v2/Program.cs
@@ -28,28 +28,26 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: "DebugPolicy", policy =>
-    {
+builder.Services.AddCors(options => {
+    options.AddPolicy(name: "DebugPolicy",policy => {
         policy.SetIsOriginAllowedToAllowWildcardSubdomains()
         .SetIsOriginAllowed((host) => true)
         .AllowCredentials()
         .AllowAnyHeader()
-        .AllowAnyMethod();
+        .AllowAnyMethod()
+        .WithExposedHeaders("X-Number-of-Pages");
     });
-    options.AddPolicy(name: "FrontEndAndOriginOnly", policy =>
-    {
+    options.AddPolicy(name: "FrontEndAndOriginOnly",policy => {
         policy.SetIsOriginAllowed((host) => true)
         .WithOrigins("https://culo-t97g.vercel.app/")
         .AllowCredentials()
         .AllowAnyHeader()
-        .AllowAnyMethod();
+        .AllowAnyMethod()
+        .WithExposedHeaders("X-Number-of-Pages");
     });
 });
 builder.Services.AddControllers(options => options.Conventions.Add(new GlobalControllerRoutePrefixConvention(new GlobalControllerRoutePrefix(GLOBAL_ROUTE_PREFIX))))
-    .AddJsonOptions(options =>
-    {
+    .AddJsonOptions(options => {
         options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
     });
 ;
@@ -57,10 +55,8 @@ builder.Services.AddControllers(options => options.Conventions.Add(new GlobalCon
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(opt =>
-{
-    opt.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
-    {
+builder.Services.AddSwaggerGen(opt => {
+    opt.AddSecurityDefinition("oauth2",new OpenApiSecurityScheme {
         In = ParameterLocation.Header,
         Name = "Authorization",
         Type = SecuritySchemeType.ApiKey
@@ -73,15 +69,15 @@ builder.Services.AddSwaggerGen(opt =>
 // SERVER CONNECTIONS
 //
 
-var serverVersion = new MariaDbServerVersion(new Version(10, 4, 28));
+var serverVersion = new MariaDbServerVersion(new Version(10,4,28));
 
-builder.Services.AddDbContext<DatabaseContext>(options => options.UseMySql(builder.Configuration.GetConnectionString("ProgramDB"), serverVersion, mysqlOptions => mysqlOptions.UseNewtonsoftJson()));
-builder.Services.AddDbContext<LoggingDatabaseContext>(options => options.UseMySql(builder.Configuration.GetConnectionString("ProgramDB"), serverVersion));
-builder.Services.AddDbContext<AuthDB>(options => options.UseMySql(builder.Configuration.GetConnectionString("AUTHTESTING"), serverVersion));
-builder.Services.AddDbContext<DirectMessagesDB>(options => options.UseMySql(builder.Configuration.GetConnectionString("ProgramDB"), serverVersion));
+builder.Services.AddDbContext<DatabaseContext>(options => options.UseMySql(builder.Configuration.GetConnectionString("ProgramDB"),serverVersion,mysqlOptions => mysqlOptions.UseNewtonsoftJson()));
+builder.Services.AddDbContext<LoggingDatabaseContext>(options => options.UseMySql(builder.Configuration.GetConnectionString("ProgramDB"),serverVersion));
+builder.Services.AddDbContext<AuthDB>(options => options.UseMySql(builder.Configuration.GetConnectionString("AUTHTESTING"),serverVersion));
+builder.Services.AddDbContext<DirectMessagesDB>(options => options.UseMySql(builder.Configuration.GetConnectionString("ProgramDB"),serverVersion));
 
-builder.Services.AddDbContext<KaizenTables>(options => options.UseMySql(builder.Configuration.GetConnectionString("connection"), serverVersion));
-builder.Services.AddDbContext<InventoryAccounts>(options => options.UseMySql(builder.Configuration.GetConnectionString("connection"), serverVersion));
+builder.Services.AddDbContext<KaizenTables>(options => options.UseMySql(builder.Configuration.GetConnectionString("connection"),serverVersion));
+builder.Services.AddDbContext<InventoryAccounts>(options => options.UseMySql(builder.Configuration.GetConnectionString("connection"),serverVersion));
 
 //Use in memory db for testing
 //builder.Services.AddDbContext<KaizenTables>(options => options.UseInMemoryDatabase("DBTest"));
@@ -89,44 +85,36 @@ builder.Services.AddDbContext<InventoryAccounts>(options => options.UseMySql(bui
 
 builder.Services.AddAuthorization();
 
-builder.Services.AddIdentity<APIUsers, IdentityRole>()
+builder.Services.AddIdentity<APIUsers,IdentityRole>()
     .AddEntityFrameworkStores<AuthDB>()
     .AddDefaultTokenProviders();
-builder.Services.AddAuthentication(options =>
-{
+builder.Services.AddAuthentication(options => {
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
     options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
     options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options =>
-{
+}).AddJwtBearer(options => {
     options.SaveToken = true;
     options.RequireHttpsMetadata = false;
-    options.TokenValidationParameters = new TokenValidationParameters()
-    {
+    options.TokenValidationParameters = new TokenValidationParameters() {
         ValidateIssuer = true,
         ValidateAudience = true,
         ValidAudience = builder.Configuration.GetSection("JWT").GetValue<string>("ValidAudience"),
         ValidIssuer = builder.Configuration.GetSection("JWT").GetValue<string>("ValidIssuer"),
         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetSection("JWT").GetValue<string>("Secret")))
     };
-    options.Events = new JwtBearerEvents
-    {
-        OnMessageReceived = context =>
-        {
+    options.Events = new JwtBearerEvents {
+        OnMessageReceived = context => {
             var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
 
-            if (!string.IsNullOrEmpty(token))
-            {
+            if(!string.IsNullOrEmpty(token)) {
                 context.Token = token;
                 return Task.CompletedTask;
             }
-            else
-            {
+            else {
                 string? accessToken = context.Request.Query["access_token"];
                 var path = context.HttpContext.Request.Path;
-                if (!string.IsNullOrEmpty(accessToken) &&
-                    (path.StartsWithSegments("/live-chat")))
-                {
+                if(!string.IsNullOrEmpty(accessToken) &&
+                    (path.StartsWithSegments("/live-chat"))) {
                     context.Token = accessToken.Split(" ").Last();
                 }
                 return Task.CompletedTask;
@@ -142,17 +130,16 @@ builder.Services.AddSignalR();
 //NOTE: Services can also be dependency injected
 //E.G: Using a DBContext in the constructor to get a database context
 
-builder.Services.AddScoped<IDataAccess, DataAccess>();
+builder.Services.AddScoped<IDataAccess,DataAccess>();
 builder.Services.AddScoped<NotificationService>();
 
-builder.Services.AddScoped<IActionLogger, AccountManager>(); //Logging service
-builder.Services.AddTransient<IEmailService, EmailService>(); //Email Sending Service
-builder.Services.AddTransient<IInventoryBOMBridge, BOMInventoryBridge>(); //Inventory BOM Bridge Service
-builder.Services.AddSingleton<ILiveChatConnectionManager, LiveChatConnectionManager>(); //Live chat connections
+builder.Services.AddScoped<IActionLogger,AccountManager>(); //Logging service
+builder.Services.AddTransient<IEmailService,EmailService>(); //Email Sending Service
+builder.Services.AddTransient<IInventoryBOMBridge,BOMInventoryBridge>(); //Inventory BOM Bridge Service
+builder.Services.AddSingleton<ILiveChatConnectionManager,LiveChatConnectionManager>(); //Live chat connections
 
 builder.Services.AddRateLimiter(_ => _
-    .AddFixedWindowLimiter(policyName: "fixed", options =>
-    {
+    .AddFixedWindowLimiter(policyName: "fixed",options => {
         options.PermitLimit = 4;
         options.Window = TimeSpan.FromSeconds(12);
         options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
@@ -164,9 +151,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
 //{
-    app.UseSwagger()
-    .UseSwaggerUI()
-    .UseCors("FrontEndAndOriginOnly");
+app.UseSwagger()
+.UseSwaggerUI()
+.UseCors("FrontEndAndOriginOnly");
 //}
 
 app.UseRateLimiter();
