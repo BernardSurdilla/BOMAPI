fa9979497834fb2727b50b514dd67fee1e5f90a7
Bernard Surdilla <bernardsurdilla@outlook.com>
2024-10-26 13:29:30 +0800

    Fix buggy patch request format

--- BOM-API v2/Program.cs
+++ BOM-API v2/Program.cs
@@ -39,7 +39,7 @@ builder.Services.AddCors(options => {
     });
     options.AddPolicy(name: "FrontEndAndOriginOnly",policy => {
         policy.SetIsOriginAllowed((host) => true)
-        .WithOrigins("https://culo-t97g.vercel.app/")
+        .WithOrigins("https://culo-vite.vercel.app/")
         .AllowCredentials()
         .AllowAnyHeader()
         .AllowAnyMethod()
